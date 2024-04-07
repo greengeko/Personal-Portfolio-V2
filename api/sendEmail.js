1c8cd22add7a809e8643ee0619fce254f503907c
greengeko <mackevicar@gmail.com>
2024-04-07 13:12:16 +0200

    fix import

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,17 +1,18 @@
-const nodemailer = require('nodemailer');
+import { createTransport } from 'nodemailer';
 
+const transporter = createTransport({
+    service: 'Gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { name, email, subject, message } = req.body;
 
         try {
-            const transporter = nodemailer.createTransport({
-                service: 'Gmail',
-                auth: {
-                    user: process.env.EMAIL_USER,
-                    pass: process.env.EMAIL_PASSWORD
-                }
-            });
+
 
             const mailOptions = {
                 from: process.env.EMAIL_USER,

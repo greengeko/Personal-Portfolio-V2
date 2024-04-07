6b2889f0bed42045361d3514079c36af1e4ab637
greengeko <mackevicar@gmail.com>
2024-04-07 14:45:15 +0200

    email user

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
                 const mailOptions = {
                     from: process.env.EMAIL_USER,
-                    to: process.env.RECIPIENT_EMAIL,
+                    to: process.env.EMAIL_USER,
                     subject: subject,
                     text: `From: ${name}\nEmail: ${email}\nMessage: ${message}`
                 };
